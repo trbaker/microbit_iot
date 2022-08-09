3dd26b71fc5ab34617faa4e2abb56d3068dc86bc
Tom R Baker <trbaker@gmail.com>
2022-08-08 19:23:25 -0500

    Update computer1.py

--- computer1.py
+++ computer1.py
@@ -1,4 +1,7 @@
 import serial
+import requests
+import json
+
 
 ser = serial.Serial('/dev/cu.usbmodem143202', 115200, timeout=None)
 while True:
@@ -8,3 +11,14 @@ while True:
     tempf = tempf.replace("b'", "")
     # print to screen (for now)
     print(tempf)
+    # send data to sample ArcGIS Online service
+    url = 'https://services3.arcgis.com/GzteEaZqBuJ6GIYr/arcgis/rest/services/survey123_910b6ea1c50743269a5b171a91fe6cc7_fieldworker/FeatureServer/0/addFeatures'
+    params={"f":"pjson","token":"","rollbackOnFailure":"false","features":'{ \
+        "attributes" : { \
+            "controller" : "microbit sensor 1", \
+            "lat" : -94.23434, \
+            "lon": -94.2342, \
+            "temperature" : tempf, \
+            "humidity" : 0}}' 
+            }
+    x = requests.post(url, params=params)
